92159b29f814c652b6a968eb94e71b726a90fe5e
dkool <dennis.kool@cloudnation.nl>
2022-05-10 15:41:53 +0200

    cleanup

---
+++ outputs.tf
@@ -0,0 +1,19 @@
+# output "sa" {
+#   value = azurerm_storage_account.sa
+# }
+
+# output "sc" {
+#   value = azurerm_storage_container.sc
+# }
+
+# output "sq" {
+#   value = azurerm_storage_queue.sq
+# }
+
+# output "sh" {
+#   value = azurerm_storage_share.sh
+# }
+
+# output "st" {
+#   value = azurerm_storage_table.st
+# }
